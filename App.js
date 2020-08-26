d49aa945768a7fd43d2590eede2ebcaf2ba08f86
Deepak jha <ddjha171@gmail.com>
2020-08-26 09:55:17 +0530

    FlatList Items Created

--- App.js
+++ App.js
@@ -1,12 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Dimensions, FlatList, Image, StyleSheet, Text, View} from 'react-native';
+import Data from "./src/data/Data";
+
+const {width, height} = Dimensions.get(`window`);
+const LOGO_WIDTH = 240;
+const LOGO_HEIGHT = 40;
+const DOT_SIZE = 40;
+
+const Item = ({imageUri, heading, description}) => {
+  return (
+    <View style={styles.itemStyle}>
+      <Image source={imageUri} style={[styles.imageStyle]}/>
+      <View style={styles.textContainer}>
+        <Text style={[styles.heading]}>{heading}</Text>
+        <Text style={styles.description}>{description}</Text>
+      </View>
+    </View>
+  )
+
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' hidden/>
+      <FlatList
+        data={Data}
+        keyExtractor={(item) => item.id}
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        horizontal
+      />
+      <Image style={styles.logo}
+             source={require("../reactnativeanimation/assets/alex.jpg")}/>
+      {/*<Pagination/>*/}
     </View>
   );
 }
---
+++ src/data/Data.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    id: 1,
+    type: "ALex",
+    imageUri: require("../../assets/alex.jpg"),
+    heading: "Vibrant color",
+    color: "#9dcdfa",
+    description:"Good Alex"
+  },
+  {
+    id: 2,
+    type: "Shoes",
+    imageUri: require("../../assets/background.jpg"),
+    heading: "Black color",
+    color: "#9dcdfa",
+    description:"Good shoes"
+  },
+  {
+    id: 3,
+    type: "BattelCreek",
+    imageUri: require("../../assets/battlecreek.jpg"),
+    heading: "Vibrant color",
+    color: "#9dcdfa",
+    description:"Good shoes"
+  },
+  {
+    id: 4,
+    type: "Giogio",
+    imageUri: require("../../assets/giorgio.jpg"),
+    heading: "Vibrant color",
+    color: "#9dcdfa",
+    description:"Good shoes"
+  },
+  {
+    id: 5,
+    type: "Mae",
+    imageUri: require("../../assets/mae.jpg"),
+    heading: "Vibrant color",
+    color: "#9dcdfa",
+    description:"Good shoes"
+  },
+  {
+    id: 6,
+    type: "Malte",
+    imageUri: require("../../assets/malte.jpg"),
+    heading: "Vibrant color",
+    color: "#9dcdfa",
+    description:"Good shoes"
+  },
+  {
+    id: 7,
+    type: "Watch",
+    imageUri: require("../../assets/watch.jpg"),
+    heading: "Vibrant color",
+    color: "#9dcdfa",
+    description:"Good shoes"
+  }
+]
